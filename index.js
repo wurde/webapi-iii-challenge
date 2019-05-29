185c61997ebe4a191121873977fb02eec200beed
Andy Bettisworth <andy@accreu.com>
2019-05-29 12:41:04 -0500

    Setup basic server

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// code away!
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const express = require('express')
+
+/**
+ * Constants
+ */
+
+const port = 8080
+
+/**
+ * Define app
+ */
+
+const app = express()
+
+/**
+ * Routes
+ */
+
+app.get('/', (req, res) => {
+  res.sendStatus(200)
+})
+
+/**
+ * Start server
+ */
+
+if (module === require.main) {
+  app.listen(port, () => {
+    console.log(`Express server running on ${port}`)
+  })
+}
+
+/**
+ * Export app
+ */
+
+module.exports = app
